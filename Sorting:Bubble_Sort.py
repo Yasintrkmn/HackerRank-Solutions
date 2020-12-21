672f6b962264015ce7fe376ef799a83fa4dab562
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2020-12-21 16:16:02 +0300

    Create Sorting:Bubble_Sort.py

---
+++ Sorting:Bubble_Sort.py
@@ -0,0 +1,26 @@
+# Diziyi büyükten küçüğe doğru sıralama
+# Örneğin input= [4,1,2,3] -> output=[1,2,3,4,5]
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countSwaps function below.
+def countSwaps(a):
+    count=0
+    for i in range(len(a)):
+        for j in range(len(a)-1):
+            if a[j] > a[j+1]:
+                a[j],a[j+1] = a[j+1],a[j]
+                count+=1
+    print("Array is sorted in {} swaps.".format(count))
+    print("First Element: {}".format(a[0]))
+    print("Last Element: {}".format(a[-1]))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
