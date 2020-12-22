c56ffc1c15b30cf52f0ef5a6863ba5626e641c51
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2020-12-22 15:57:56 +0300

    Create Simple_Array_Sum.py

---
+++ Simple_Array_Sum.py
@@ -0,0 +1,9 @@
+# Bir listedeki elemanların toplamını elde etme
+
+def simpleArraySum(ar):
+    return sum(ar)
+if __name__ == '__main__':
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    print(result)
