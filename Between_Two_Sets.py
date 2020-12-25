e287a50be491e44af6f7be2cbd174cd704f5bfc4
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2020-12-25 16:44:58 +0300

    Create Between_Two_Sets.py

---
+++ Between_Two_Sets.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from math import gcd
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    liste=[]
+    liste2=[]
+    lcm = a[0]
+    for i in a[1:]:
+        lcm = lcm * i // gcd(lcm, i)
+
+    for i in range(lcm,min(b)+1,lcm):
+        liste.append(i)
+
+    count=0
+    print(liste)
+    for j in liste:
+        count=0
+        for k in b:
+            if k%j !=0:
+                count=1
+                continue
+        if count==0:
+            liste2.append(j)
+            
+    return len(liste2)
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+    n = int(first_multiple_input[0])
+    m = int(first_multiple_input[1])
+    arr = list(map(int, input().rstrip().split()))
+    brr = list(map(int, input().rstrip().split()))
+    total = getTotalX(arr, brr)
+    print(total)
+   
