ce0403ed792c3c74d21bcb816ae11ba2167dbfd5
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2020-12-25 16:46:56 +0300

    Update Between_Two_Sets.py

--- Between_Two_Sets.py
+++ Between_Two_Sets.py
@@ -1,19 +1,4 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
 from math import gcd
-#
-# Complete the 'getTotalX' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY a
-#  2. INTEGER_ARRAY b
-#
 
 def getTotalX(a, b):
     liste=[]
