3b0d5ced950a2d8b8ffdbc4e7d8be670d997f7ef
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2021-03-15 20:16:16 +0300

    Create Angry_Professor.py

---
+++ Angry_Professor.py
@@ -0,0 +1,19 @@
+def angryProfessor(k, a):
+    return "NO" if sum(1 for i in a if i<=0)>=k else "YES"
+  
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        print(result)
