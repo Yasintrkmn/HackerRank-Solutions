8bf26fd84690d0bce598bf26a538153e51887934
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2021-05-07 11:04:50 +0300

    Create Fair_Rations.py

---
+++ Python/Fair_Rations.py
@@ -0,0 +1,33 @@
+#The problem: https://www.hackerrank.com/challenges/fair-rations/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+def fairRations(B):
+    count = 0
+    for i in range(len(B) - 1):
+        if B[i] % 2 != 0:
+            B[i] += 1
+            B[i + 1] += 1
+            count += 2
+            
+    if sum(B) % 2 == 1:
+            return "NO"
+
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    N = int(input())
+
+    B = list(map(int, input().rstrip().split()))
+
+    result = fairRations(B)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
