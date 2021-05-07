b7bb566a456817edbd43c7da3b55c3f8b6de78f2
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2021-05-07 11:03:21 +0300

    Create Equalize_the_Array.py

---
+++ Python/Equalize_the_Array.py
@@ -0,0 +1,27 @@
+# The problem: https://www.hackerrank.com/challenges/equality-in-a-array/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+def equalizeArray(arr):
+    x=0
+    x = [arr.count(i) for i in range(min(arr),max(arr)+1) if arr.count(i)>x]
+
+    return len(arr)-max(x)
+            
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
